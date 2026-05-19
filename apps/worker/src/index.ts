753e4e9f0b2af7847e6c462f76d777ccdd21f74d
Sanghawk <sanghawk@gmail.com>
2026-05-18 22:35:38 -0700

    chore: create dtabase package skeleton

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,5 +1,6 @@
+import { dbClientPlaceholder } from '@base-launch-intelligence/db';
 import type { ChainId } from '@base-launch-intelligence/shared';
 
 const baseChainId: ChainId = 8453;
 
-export { baseChainId };
+export { baseChainId, dbClientPlaceholder };
